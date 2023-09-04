e17faf78497e1b765c4c0f5df6451066d7ba0ca1
anprivitera <andrea.privitera@mailbox.org>
2023-09-03 18:08:14 -0700

    wrote action plan

--- generator.js
+++ generator.js
@@ -2,7 +2,7 @@
 It includes: 
 - Ascending Armor class
 - HD
-- Attribute scorse
+- Attribute scores with modifiers
 - Name 
 - Class
 - Level
@@ -18,4 +18,19 @@ It includes:
 - Coins
 - Languages
 - To-Hit bonus
+- Number of hirelings and loyalty
+- Weight carried and movement speed
 */
+
+
+// Define dice roller (3d6)
+// Define Strengths, Dexterity, Constitution, Intelligence, Wisdom, Charisma. For each of them, roll 3d6 separately
+// Give an appropriate modifier for each score (3-6 = -1, 7-14 =0, 15-18 = +1)
+// Give 5% xp bonus if WIS is 15+, and 5% if CHA is 15+
+// HD Dice is 1d6
+// Choose an alignment randmoly between Law, Neutrality and Chaos
+// based on the highest score, choose the most appropriate class
+// include class characteristics
+// roll for initial money
+// pick elements from list of items randomly until money runs out (giving priority to 1 allowed weapon, 1 allowed armor and then assigning the rest randomly)
+// calculate movement speed
