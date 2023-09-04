0dc248b42a6360cd9c9ac72166df00502c5c955f
anprivitera <andrea.privitera@mailbox.org>
2023-09-03 17:55:44 -0700

    user story

--- generator.js
+++ generator.js
@@ -1,1 +1,21 @@
-document.write("Testing the script")
+/* USER STORY: I see a short textual description Whitebox character randomly generated and ready to play. 
+It includes: 
+- Ascending Armor class
+- HD
+- Attribute scorse
+- Name 
+- Class
+- Level
+- Race
+- Alignment
+- Initial XP
+- XP bonus
+- A character portrait
+- Saving throw and mods
+- Spells / Abilities
+- Equipment
+- Weapons & Armor
+- Coins
+- Languages
+- To-Hit bonus
+*/
