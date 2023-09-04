eb911e8f6b703792041320469df8b71e6a2bf720
anprivitera <andrea.privitera@mailbox.org>
2023-09-03 18:55:50 -0700

    added modifiers

--- generator.js
+++ generator.js
@@ -32,15 +32,39 @@ function diceRoller (numberOfDice) {
     return rollResult
 }
 
+// Give an appropriate modifier for each score (3-6 = -1, 7-14 =0, 15-18 = +1)
+function determineModifier(attributeScore) {
+    if (attributeScore <= 6) {
+        return -1;
+    } else if (attributeScore >= 7 && attributeScore <= 14) {
+        return 0;
+    } else {
+        return +1;
+    }
+} 
+
+const ROLL_FOR_STRENGTH = diceRoller(3),
+    ROLL_FOR_DEXTERITY = diceRoller(3),
+    ROLL_FOR_CONSTITUTION = diceRoller(3),
+    ROLL_FOR_INTELLIGENCE = diceRoller(3),
+    ROLL_FOR_WISDOM = diceRoller(3),
+    ROLL_FOR_CHARISMA = diceRoller(3),
+    STRENGTH_MODIFIER =  determineModifier(ROLL_FOR_STRENGTH),
+    DEXTERITY_MODIFIER =  determineModifier(ROLL_FOR_DEXTERITY),
+    CONSTITUTION_MODIFIER =  determineModifier(ROLL_FOR_CONSTITUTION),
+    INTELLIGENCE_MODIFIER = determineModifier(ROLL_FOR_INTELLIGENCE),
+    WISDOM_MODIFIER = determineModifier(ROLL_FOR_WISDOM),
+    CHARISMA_MODIFIER = determineModifier(ROLL_FOR_CHARISMA);
+
 let stringToDisplay = "";
 // Define Strengths, Dexterity, Constitution, Intelligence, Wisdom, Charisma. For each of them, roll 3d6 separately
 const ATTRIBUTES = [ 
-    { attributeName:"STR", attributeValue:diceRoller(3) }, 
-    { attributeName:"DEX", attributeValue:diceRoller(3) }, 
-    { attributeName:"CON", attributeValue:diceRoller(3) }, 
-    { attributeName:"INT", attributeValue:diceRoller(3) },
-    { attributeName:"WIS", attributeValue:diceRoller(3) },
-    { attributeName:"CHA", attributeValue:diceRoller(3) }
+    { attributeName:"STR", attributeValue:ROLL_FOR_STRENGTH, modifierValue: }, 
+    { attributeName:"DEX", attributeValue:ROLL_FOR_DEXTERITY }, 
+    { attributeName:"CON", attributeValue:ROLL_FOR_CONSTITUTION }, 
+    { attributeName:"INT", attributeValue:ROLL_FOR_INTELLIGENCE },
+    { attributeName:"WIS", attributeValue:ROLL_FOR_WISDOM },
+    { attributeName:"CHA", attributeValue:ROLL_FOR_CHARISMA }
 ];
 
 for (let n = 0; n < ATTRIBUTES.length; n++) {
@@ -48,7 +72,6 @@ for (let n = 0; n < ATTRIBUTES.length; n++) {
 }
 
 document.getElementById("generator").innerHTML = stringToDisplay;
-// Give an appropriate modifier for each score (3-6 = -1, 7-14 =0, 15-18 = +1)
 // Give 5% xp bonus if WIS is 15+, and 5% if CHA is 15+
 // HD Dice is 1d6
 // Choose an alignment randmoly between Law, Neutrality and Chaos
