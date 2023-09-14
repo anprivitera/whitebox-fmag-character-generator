f85356a22d9bdfb8d304ca24f5aa7c47139c1c2f
anprivitera <andrea.privitera@mailbox.org>
2023-09-14 10:24:26 -0700

    renamed

--- diceRoller.js
+++
@@ -1,9 +0,0 @@
-function diceRoller(numberOfDice) {
-  let rollResult = null;
-  for (let i = 0; i < numberOfDice; i++) {
-    rollResult += Math.floor(Math.random() * 6 + 1);
-  }
-  return rollResult;
-}
-
-export { diceRoller };
